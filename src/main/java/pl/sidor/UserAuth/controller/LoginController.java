29e818b56f572d4ed192efc190ff68702b59542b
KursJava <kursjava1@wp.pl>
2019-06-20 19:29:48 +0200

    add_security_token

--- src/main/java/pl/sidor/UserAuth/controller/LoginController.java
+++ src/main/java/pl/sidor/UserAuth/controller/LoginController.java
@@ -1,14 +1,20 @@
 package pl.sidor.UserAuth.controller;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import models.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.sidor.UserAuth.service.UserService;
 import pl.sidor.UserAuth.token.UserToken;
 
-import java.util.Optional;
+import java.util.*;
 
 @RestController
 public class LoginController {
@@ -27,5 +33,24 @@ public class LoginController {
         Optional<User> byEmailAndPassword = Optional.of(userService.login(name, password).get());
 
         return UserToken.createTokenForUser(byEmailAndPassword.get());
+    }
+    @PostMapping("/login")
+    public String login(){
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String name = authentication.getName();
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+
+        Map<String, Object> map= new HashMap<>();
+        map.put("roles", authorities);
+        map.put("name", name);
+
+        return Jwts.builder()
+               .setSubject(name)
+               .addClaims(map)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+2000))
+                .signWith(SignatureAlgorithm.HS256,"token")
+                .compact();
     }
 }
